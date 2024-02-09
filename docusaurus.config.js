6dffe91c06218817d7ec8f761f78ca120ae2c5ea
Aboubacar Doucoure <yelounak@gmail.com>
2024-02-09 12:05:32 +0100

    added faq to root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -190,6 +190,12 @@ const config = {
             position: 'right',
             label: 'Reference',
           },
+          {
+            type: 'doc',
+            docId: 'faq',
+            position: 'right',
+            label: 'FAQ 💡',
+          },
           // {
           //   type: 'doc',
           //   docId: 'commands/install',
