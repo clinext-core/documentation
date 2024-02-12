49739d78c63f22b4becdac24b4c929f82495e16d
Aboubacar Doucoure <yelounak@gmail.com>
2024-02-12 11:51:54 +0100

    improved feature block

--- src/components/project/block/feature.js
+++ src/components/project/block/feature.js
@@ -1,22 +1,22 @@
 import React from 'react'
-import clsx from 'clsx'
 import { useColorMode } from '@docusaurus/theme-common'
 
 export default ({ Svg, title, description }) => {
   const { isDarkTheme } = useColorMode()
 
   return (
-    <div className={clsx('col col--4 mb-16 md:mb-0')}>
-      <div className="text--center">
+    <div className={'col mb-16 md:mb-0'}>
+      <div className="text-center ">
         <Svg
           fill={`${isDarkTheme ? 'white' : 'black'}`}
           className={`                         
           px-10
-          w-[180px]
-          h-[180px]`}
+          w-[160px]
+          h-[120px]
+          md:h-[160px]`}
           role="img" />
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="text-center">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
